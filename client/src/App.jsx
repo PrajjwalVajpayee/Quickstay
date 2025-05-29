c26f3314dffdf3a915409c01e215654bf25b0733
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-05-30 00:00:37 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-100'>
+    <div className='w-40 mt-100 h-10 bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-semibold'>
+      
+    </div>
+    </div>
+  )
+}
+
+export default App
