286ff310296f87d66a59fdd7f42069225771373c
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-06-01 04:31:49 +0530

    added all the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+ 
   return (
-    <div className='flex items-center justify-center h-screen bg-gray-100'>
-    <div className='w-40 mt-100 h-10 bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-semibold'>
-      
-    </div>
+    <div >
+    {
+     !isOwnerPath && <Navbar/>}
+    { false && <HotelReg/>}
+     <div className='min-h-[70vh]'>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rooms" element={<AllRooms />} />
+      <Route path="/room/:id" element={<RoomDetails />} />
+      <Route path="/my-bookings" element={<MyBookings />} />
+       <Route path='/owner' element={<Layout/>}>
+            <Route index element ={<Dashboard/>}/>
+            <Route path='add-room' element={<AddRoom/>}/>
+            <Route path='list-room' element={<ListRoom/>}/>
+       </Route>
+    </Routes>
+     </div>
+     <Footer/>
     </div>
   )
 }
